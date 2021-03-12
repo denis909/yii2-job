e0cd0e5ec720e7134af5f6dafb80237b817fabb5
Denis Po <denis909@mail.ru>
2021-03-12 12:50:29 +0300

    n/a

--- Job.php
+++ Job.php
@@ -7,13 +7,15 @@ use Yii;
 abstract class Job extends \yii\base\BaseObject implements \yii\queue\JobInterface
 {
 
+    public $queueComponent = 'queue';
+
     abstract public function execute($queue);
 
     public function push($queue)
     {
         if (!$queue)
         {
-            $queue = Yii::$app->queue;
+            $queue = Yii::$app->{$this->queueComponent};
         }
 
         $queue->push($this);
